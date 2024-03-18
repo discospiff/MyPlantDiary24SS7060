c444aadd41784d6a0a3ec3c6b9737dae7c611045
Brandan Jones <jonesbr@ucmail.uc.edu>
2024-03-18 09:51:55 -0400

    Update plant class

--- app/src/main/java/app/plantdiary/myplantdiary23SS002/dto/Plant.kt
+++ app/src/main/java/app/plantdiary/myplantdiary23SS002/dto/Plant.kt
@@ -1,5 +1,5 @@
 package app.plantdiary.myplantdiary23SS002.dto
 
-data class Plant(var plantId : Int  = 0, var genus : String = "", var species : String = "") {
+data class Plant(var plantId : String  = "", var genus : String = "", var species : String = "") {
 
 }
